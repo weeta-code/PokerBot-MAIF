7cac2782617b83156672e46f346746e661f93614
Jun-L04 <LuJunyang04@gmail.com>
2025-11-10 18:03:20 -0500

    basic entry point in main
    - very basic state transition logic and framework
    - player configuration stuff

---
+++ build/CMakeFiles/PokerBotMAIF.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PokerBotMAIF.dir/src/main.cpp.o"
+  "CMakeFiles/PokerBotMAIF.dir/src/main.cpp.o.d"
+  "CMakeFiles/PokerBotMAIF.dir/src/odds_calculator.cpp.o"
+  "CMakeFiles/PokerBotMAIF.dir/src/odds_calculator.cpp.o.d"
+  "CMakeFiles/PokerBotMAIF.dir/src/risk_profiler.cpp.o"
+  "CMakeFiles/PokerBotMAIF.dir/src/risk_profiler.cpp.o.d"
+  "bin/PokerBotMAIF"
+  "bin/PokerBotMAIF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PokerBotMAIF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

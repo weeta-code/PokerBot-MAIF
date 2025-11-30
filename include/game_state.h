7a0bcf2161ebbe288c47169502bbdb42fd8319a7
Jun-L04 <LuJunyang04@gmail.com>
2025-11-29 23:00:02 -0500

    game state for poker engine
    - game_state.h has necessary functions and structs for the game state
    - game_state.cpp implements game_state.h
    - main.cpp has a boilerplate logic to run the game
    - still has some TODOs, needs integration testing

--- include/game_state.h
+++ include/game_state.h
@@ -3,52 +3,91 @@
 
 #include <vector>
 #include <string>
-#include <array>
-#include "risk_profiler"
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include "equity.h"        
+#include "risk_profiler.h" 
+//#include "odds_calculator.h" 
+
+using namespace std;
 
 enum class PlayMode {
     AUTONOMOUS,
     GUIDED
 };
 
-typedef struct {
-    std::vector<PlayerProfile> player_list;
-    StackTracker stack_tracker;
-    PlayMode play_mode;
-    // TODO keep track of hands
-    int num_players;
-    int round_number;
+enum class Stage {
+    PREFLOP,
+    FLOP,
+    TURN,
+    RIVER,
+    SHOWDOWN,
+};
 
-    void init_myself() {
-        cout >> "Choose Your Play Style: 0 for Autonomous, 1 for Guided. \n";
-        cin << play_mode;
-        cout >> "Enter Your Risk Tolerance: 0 to 1 as Decimal. \n";
-        cin << player_list[0].aggression_frequency
-    }
-
-    void init_others() {
-        // default profiles
-        for(int i = 0; i < num_players - 1; i++) {
-            PlayerProfile player;
-            player.aggression_frequency = 0.5;
-            player.bluff_frequency = 0.0;          
-            player.avg_bet_size_ratio = 0.0;        
-            player.hands_observed = 0;  
-            player_list.push_back(player);
-        }
-    }
-
-    void init_game_state() {
-        cout << "Enter the Total Number of Players. \n";
-        cin >> num_players;
-        // init myself
-        init_myself();
-        round_number = 0;
-    }
-
-    void update_other_players() { 
-        // TODO update other players throughout the game for their behaviors
-        return;
-    }
+// defining a comprehensive player profile
+struct Player {
+    string id; 
+    vector<Card> hole_cards;
+    int stack; 
+    int current_bet; // amount to bet in the current street
+    bool is_folded;
+    bool is_all_in;
+    bool is_human;
     
-} GameState;
+    // useful stats for risk profiling
+    int times_folded;
+    int times_raised;
+    int times_called;
+    int hands_played;
+    
+    PlayerProfile profile;
+
+    Player(string _id, double _stack, bool _is_human);
+};
+
+struct GameState {
+    vector<Player> players;
+    vector<Card> deck;
+    vector<Card> community_cards;
+    
+    PlayMode play_mode;
+    Stage stage;
+    int pot_size;
+    int current_street_highest_bet;
+    
+    int num_players;
+    int dealer_index;
+    int current_player_index;
+
+    // incapsulate other modules to external engines
+    RiskProfiler* risk_profiler;
+    OddsCalculator* odds_calculator;
+
+    GameState(RiskProfiler* rp, OddsCalculator* oc);
+
+    // init funcs
+    void init_game_setup(); 
+    void init_deck();
+    void shuffle_deck();
+    
+    // step by step in a street
+    void start_hand();
+    void deal_community_cards(); // for flop/turn/river
+    void next_street();
+    void resolve_winner();
+    bool is_hand_over();
+    bool is_betting_round_over();
+
+    // action func
+    Card draw_card();
+    // returns true if action is valid
+    bool record_action(int player_idx, string action, double amount);
+    
+    // helper funcs
+    int get_active_player_count();
+    Player* get_current_player();
+    void next_player();
+};
+
+#endif
